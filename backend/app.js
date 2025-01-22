9ffbd99b1b6d0e859265032bc43ecdb648790d76
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-01-23 04:59:39 +0530

    captain all routes done

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(cookiesParser());
 app.use(express.urlencoded({extended:true}));
 const userRouter=require("./routes/user.routes");
+const captainRouter=require("./routes/captain.routes");
 app.use('/user',userRouter)
+app.use('/captain',captainRouter);
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
